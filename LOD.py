aff99d74f3d4455cb79272ab78d00fcb35ae0bd1
Zhao-Xiaodan <zhaoxiaodan1984@gmail.com>
2022-10-07 13:41:45 +0800

    error scatter plot

--- LOD.py
+++ LOD.py
@@ -11,32 +11,48 @@ from sklearn.metrics import mean_squared_error, r2_score
 2022-08-30 using repeats for 4 times, so take mean 4 wells for 1 test
 2022-10-06 one 1 well for 1 test
 '''
+def dataMean(data, nRow):
+    data = data.reshape(nRow, -1)
+    data = np.mean(data, axis=1)
+    data = data.reshape(-1, 1)
+    return data
+
 conc = np.loadtxt('2022-08-30_POCT_Conc.txt')
-#  conc = np.loadtxt('concentration.txt')
-SMS = np.loadtxt('2022-08-30_POCT_data.txt')
-selectNum = -1
+conc = conc.reshape(-1, 1)
+nRow = len(conc)
 
-#  import pdb; pdb.set_trace()
+data_1st = np.loadtxt('2022-08-30_POCT_data.txt')
+data_2nd = np.loadtxt('2022-10-06_CR3022 Spike-in in RIPA_2nd.txt')
+data_3rd = np.loadtxt('2022-10-06_CR3022 Spike-in in RIPA_3rd.txt')
+
+data_1st = dataMean(data_1st, nRow)
+data_2nd = dataMean(data_2nd, nRow)
+data_3rd = dataMean(data_3rd, nRow)
+
+data = np.hstack((data_1st, data_2nd, data_3rd))
+#  data = dataMean(data, nRow)
+selectNum = 3
 conc = conc[0:selectNum]
-SMS = SMS[0:selectNum*4, :]
-#  conc = conc[selectNum:]
-#  SMS = SMS[selectNum:, :]
+data = data[0:selectNum]
 
-print(conc)
-#  print(SMS)
+#  import pdb; pdb.set_trace()
+#  data_1st = data_1st[0:selectNum*4, :]
+#  conc = conc[selectNum:]
+#  data_1st = data_1st[selectNum:, :]
+
+#  print(conc)
+#  print(data_1st)
 #  import pdb; pdb.set_trace()
 
-concData = np.tile(np.array([conc]), (1, 16))
+#  concData = np.tile(np.array([conc]), (1, 16))
 #  concData = np.tile(np.array([conc]).transpose(), (1, 16))
-print(concData)
 
-breakpoint()
-#  print(f"transpose(1,4){ concData }")
-SMS = SMS.flatten().reshape(-1, 1)
-#  SMS = SMS.flatten()
-#  print(f"flatten and reshape{SMS}")
 #  breakpoint()
-concData = concData.flatten().reshape(-1, 1)
+#  print(f"transpose(1,4){ concData }")
+#  data_1st = data_1st.flatten().reshape(-1, 1)
+#  data_1st = data_1st.flatten()
+#  print(f"flatten and reshape{data_1st}")
+#  concData = concData.flatten().reshape(-1, 1)
 #  import pdb; pdb.set_trace()
 
 # Create linear regression object
@@ -44,13 +60,13 @@ regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
 #  regr.fit(diabetes_X_train, diabetes_y_train)
-regr.fit(concData, SMS)
+regr.fit(conc, data)
 #  import pdb; pdb.set_trace()
 
 # Make predictions using the testing set
-SMS_y_pred = regr.predict(concData)
+data_y_pred = regr.predict(conc)
 
-dev = SMS - SMS_y_pred
+dev = data - data_y_pred
 SD = np.mean(dev ** 2)
 LOD = 3.3 * SD / regr.coef_
 LOD = LOD[0][0]
@@ -59,18 +75,18 @@ LOD = LOD[0][0]
 print('LOD: \n', LOD)
 # The coefficients
 print("Coefficients: \n", regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(concData, SMS))
-# The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(SMS, SMS_y_pred))
-
 # Plot outputs
-plt.style.use('seaborn')
 
-plt.scatter(concData, SMS, color="black")
-plt.plot(concData, SMS_y_pred, color="blue", linewidth=3)
+sns.set_theme(style='darkgrid')
+print(conc)
+conc = conc.flatten()
+print(conc)
+print(data)
+plt.scatter(conc, data, color="black")
+#  breakpoint()
+plt.plot(conc, data_y_pred, color="blue", linewidth=3)
 plt.xlabel('Spike Concentration (pM)')
-plt.text(np.mean(conc), max(SMS), 'LOD: 3.3x$\u03C3$/slop = '+ ' '+ str(round(LOD, 2)),\
+plt.text(np.mean(conc), max(data), 'LOD: 3.3x$\u03C3$/slop = '+ ' '+ str(round(LOD, 2)),\
           horizontalalignment='center', size='x-large', color='k', weight='semibold')
 
 plt.ylabel('Bead number per 100 X 100 $\mu$$m^2$')
@@ -80,3 +96,4 @@ plt.ylabel('Bead number per 100 X 100 $\mu$$m^2$')
 
 plt.show()
 
+breakpoint()
